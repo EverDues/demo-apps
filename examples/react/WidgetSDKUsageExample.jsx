1bbbd88f3f29ea4af38d59624770520b1fcf116e
bandrivskyi <bandrivskiy96@gmail.com>
2023-03-16 16:14:19 +0200

    chore: add readme& add check example

--- examples/react/WidgetSDKUsageExample.jsx
+++ examples/react/WidgetSDKUsageExample.jsx
@@ -25,3 +25,49 @@ export const WidgetSDKUsageExample = () => {
     </>
   )
 }
+
+export const WidgeURLUsageExample = () => {
+  // URL example
+  const widgetUrl =
+    'https://subscribe.everdues.com/?service=<service_id>&tariff=<tariff>&redirect=https://my_site_success_page&theme=<light|dark>'
+
+  return (
+    <div>
+      <span>Pro</span>
+      <span>Price 1$</span>
+      <span>Period 1 mounth</span>
+      <a href="https://subscribe.everdues.com/?service=<service_id>&tariff=<tariff>&redirect=https://my_site_success_page&theme=<light|dark>">
+        Subscribe
+      </a>
+    </div>
+  )
+}
+
+export const CheckIfUserSubscribed = () => {
+  
+  const service = '0:0000000000000000000'
+  const email = 'example@mail.com'
+  const [isSubscribed, setIsSubscribed] = useState(false)
+
+  useEffect(() => {
+    void (async function () {
+      const subscribed = await EverDuesClient.subscription.checkIfSubscribedByEmail(service, email)
+      setIsSubscribed(subscribed)
+    })()
+  }, [])
+
+  return (
+    <div>
+      <span>Pro</span>
+      <span>Price 1$</span>
+      <span>Period 1 mounth</span>
+      {isSubscribed ? (
+        <a href="https://subscribe.everdues.com/?service=<service_id>&tariff=<tariff>&redirect=https://my_site_success_page&theme=<light|dark>">
+          Subscribe
+        </a>
+      ) : (
+        <>User already subscribed</>
+      )}
+    </div>
+  )
+}
